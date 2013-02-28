805fbb4c8ebf54db15ad64bd63a04bdacf236e52
Luciano Ramalho <luciano@ramalho.org>
2013-02-27 21:51:54 -0300

    chat multi-salas funcionando

--- chat/multi/server.js
+++ chat/multi/server.js
@@ -55,8 +55,8 @@ server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
 
-//var chatServer = require('./lib/chat_server');
-//chatServer.listen(server);
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
 
 
 
